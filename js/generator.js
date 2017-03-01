feb8c1567a9954d02060facb71d56e4eaf358441
Daniel Sousa <sousa.danielg@gmail.com>
2017-03-01 12:42:15 -0800

    made some adjustments

--- js/generator.js
+++ js/generator.js
@@ -63,27 +63,19 @@ var view = {
   	},
 
   	changeColor: function() {	
-	    let random = Math.floor(Math.random() * 5) + 1
-	    view.$body.removeClass();
-	    view.$rightBtn.removeClass();
-	    view.$leftBtn.removeClass();
-	    view.$quoteText.removeClass();
-	    view.$authorText.removeClass();
-	    view.$marks.removeClass();
-	    view.$body.addClass('color' + random);
-	    view.$rightBtn.addClass('color' + random);
-	    view.$leftBtn.addClass('color' + random);
-	    view.$quoteText.addClass('fontColor' + random);
-	    view.$authorText.addClass('fontColor' + random);
-	    view.$marks.addClass('fontColor' + random);
+	    let random = Math.floor(Math.random() * 5) + 1;
+	    let colorChanges = ["$body", "$rightBtn", "$leftBtn", "$quoteText", "$authorText", "$marks"]
+	    colorChanges.forEach(function(element, i) {
+	    	view[element].removeClass().addClass((i <= 2 ? "color": "fontColor") + random); 
+	    })
 	},
 
 	animate: function() {
 	    let random = Math.floor(Math.random() * 8)
-	    const animationArray = ['animated rotateIn', 'animated zoomInDown','animated hinge','animated bounce','animated shake','animated rubberBand','animated swing','animated wobble']
-	    view.$main.addClass(animationArray[random]);
+	    const animationArray = ['rotateIn', 'zoomInDown','hinge','bounce','shake','rubberBand','swing','wobble']
+	    view.$main.addClass("animated " + animationArray[random]);
 	    setTimeout(function() {
-	      view.$main.removeClass(animationArray[random]);
+	      view.$main.removeClass("animated " + animationArray[random]);
 	    }, 800);
  	},
 
