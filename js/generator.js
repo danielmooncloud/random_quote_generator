e7ba50691719dc9faf1043924599a9bd334ac861
dgsousa <sousa.danielg@gmail.com>
2017-03-18 18:19:50 -0700

    es6-ify

--- js/generator.js
+++ js/generator.js
@@ -1,31 +1,31 @@
 (function() {
 'use strict';
 
-var model = {
-	
-	getNewQuote: function(callback) {    
-	    $.getJSON('data/quotes.json', function(data) {
+const model = {
+	getNewQuote(callback) {    
+	    $.getJSON('data/quotes.json', (data) => {
 	    	callback(data)
 	    });
   	} 
 }
 
-var controller = {
-	init: function() {
+const controller = {
+	init() {
 		view.init();
 		this.refreshQuote();	
 	},
-	refreshQuote: ()=> model.getNewQuote(view.renderNew)
+	refreshQuote() {model.getNewQuote(view.renderNew)} 
+	
 }
 
-var view = {
-	init: function() {
+const view = {
+	init() {
 		this.cacheDom();
 		this.bindEvents();
 		this.changeColor();
 		this.animate();
 	},
-	cacheDom: function() {
+	cacheDom() {
 		this.$main = $('.main');
 		this.$body = $('body');
 		this.$rightBtn = this.$main.find('#right-button');
@@ -35,44 +35,41 @@ var view = {
     	this.$marks = this.$main.find('#marks');	
     	this.$twitter = this.$main.find('.twitter');
 	},
-	bindEvents: function() {
-		this.$rightBtn.click(function() {
+	bindEvents() {
+		this.$rightBtn.click(()=> {
 			controller.refreshQuote();	
 			view.changeColor();
 			view.animate();
 		})
 	},
-	renderNew: function(data) {
-		let random = Math.floor(Math.random() * 50)
-	    let currentQuote = data[random].quote;
-	    let currentAuthor = data[random].person;                
+	renderNew(data) {
+		const random = Math.floor(Math.random() * 50)
+	    const currentQuote = data[random].quote;
+	    const currentAuthor = data[random].person;                
      	view.tweet('"' + currentQuote + '"     ' + currentAuthor); 
 	    view.$quoteText.html(currentQuote);
 	    view.$authorText.html(currentAuthor);    
   	},
-  	changeColor: function() {	
-	    let random = Math.floor(Math.random() * 5) + 1;
-	    let colorChanges = ["$body", "$rightBtn", "$leftBtn", "$quoteText", "$authorText", "$marks"]
+  	changeColor() {	
+	    const random = Math.floor(Math.random() * 5) + 1;
+	    const colorChanges = ["$body", "$rightBtn", "$leftBtn", "$quoteText", "$authorText", "$marks"]
 	    colorChanges.forEach(function(element, i) {
 	    	view[element].removeClass().addClass((i <= 2 ? "color": "fontColor") + random); 
 	    })
 	},
-	animate: function() {
-	    let random = Math.floor(Math.random() * 8)
+	animate() {
+	    const random = Math.floor(Math.random() * 8)
 	    const animationArray = ['rotateIn', 'zoomInDown','hinge','bounce','shake','rubberBand','swing','wobble']
 	    view.$main.addClass("animated " + animationArray[random]);
 	    setTimeout(function() {
 	      view.$main.removeClass("animated " + animationArray[random]);
 	    }, 800);
  	},
- 	tweet: (string)=> view.$twitter.attr("href", "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent(string))         
-  	
+ 	tweet(string) {view.$twitter.attr("href", "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent(string));}
 }
 
 $(document).ready(function() {
 	controller.init();
 })
-
-
 })();
 
