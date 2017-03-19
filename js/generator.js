7497ef20d545da2742d4b89574ecabb928949b73
dgsousa <sousa.danielg@gmail.com>
2017-03-18 18:44:49 -0700

    es6-ify and eslint

--- js/generator.js
+++ js/generator.js
@@ -1,75 +1,74 @@
 (function() {
-'use strict';
+	"use strict";
 
-const model = {
-	getNewQuote(callback) {    
-	    $.getJSON('data/quotes.json', (data) => {
-	    	callback(data)
-	    });
-  	} 
-}
+	const model = {
+		getNewQuote(callback) {    
+			$.getJSON("data/quotes.json", (data) => {
+				callback(data);
+			});
+		} 
+	};
 
-const controller = {
-	init() {
-		view.init();
-		this.refreshQuote();	
-	},
-	refreshQuote() {model.getNewQuote(view.renderNew)} 
-	
-}
+	const controller = {
+		init() {
+			view.init();
+			this.refreshQuote();	
+		},
+		refreshQuote() {model.getNewQuote(view.renderNew);} 
+	};
 
-const view = {
-	init() {
-		this.cacheDom();
-		this.bindEvents();
-		this.changeColor();
-		this.animate();
-	},
-	cacheDom() {
-		this.$main = $('.main');
-		this.$body = $('body');
-		this.$rightBtn = this.$main.find('#right-button');
-		this.$leftBtn = this.$main.find('#left-button');
-		this.$quoteText = this.$main.find('#quotetext');
-    	this.$authorText = this.$main.find('#authortext');
-    	this.$marks = this.$main.find('#marks');	
-    	this.$twitter = this.$main.find('.twitter');
-	},
-	bindEvents() {
-		this.$rightBtn.click(()=> {
-			controller.refreshQuote();	
-			view.changeColor();
-			view.animate();
-		})
-	},
-	renderNew(data) {
-		const random = Math.floor(Math.random() * 50)
-	    const currentQuote = data[random].quote;
-	    const currentAuthor = data[random].person;                
-     	view.tweet('"' + currentQuote + '"     ' + currentAuthor); 
-	    view.$quoteText.html(currentQuote);
-	    view.$authorText.html(currentAuthor);    
-  	},
-  	changeColor() {	
-	    const random = Math.floor(Math.random() * 5) + 1;
-	    const colorChanges = ["$body", "$rightBtn", "$leftBtn", "$quoteText", "$authorText", "$marks"]
-	    colorChanges.forEach(function(element, i) {
-	    	view[element].removeClass().addClass((i <= 2 ? "color": "fontColor") + random); 
-	    })
-	},
-	animate() {
-	    const random = Math.floor(Math.random() * 8)
-	    const animationArray = ['rotateIn', 'zoomInDown','hinge','bounce','shake','rubberBand','swing','wobble']
-	    view.$main.addClass("animated " + animationArray[random]);
-	    setTimeout(function() {
-	      view.$main.removeClass("animated " + animationArray[random]);
-	    }, 800);
- 	},
- 	tweet(string) {view.$twitter.attr("href", "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent(string));}
-}
+	const view = {
+		init() {
+			this.cacheDom();
+			this.bindEvents();
+			this.changeColor();
+			this.animate();
+		},
+		cacheDom() {
+			this.$main = $(".main");
+			this.$body = $("body");
+			this.$rightBtn = this.$main.find("#right-button");
+			this.$leftBtn = this.$main.find("#left-button");
+			this.$quoteText = this.$main.find("#quotetext");
+			this.$authorText = this.$main.find("#authortext");
+			this.$marks = this.$main.find("#marks");	
+			this.$twitter = this.$main.find(".twitter");
+		},
+		bindEvents() {
+			this.$rightBtn.click(() => {
+				controller.refreshQuote();	
+				view.changeColor();
+				view.animate();
+			});
+		},
+		renderNew(data) {
+			const random = Math.floor(Math.random() * 50);
+			const currentQuote = data[random].quote;
+			const currentAuthor = data[random].person;                
+			view.tweet("\"" + currentQuote + "\"     " + currentAuthor); 
+			view.$quoteText.html(currentQuote);
+			view.$authorText.html(currentAuthor);    
+		},
+		changeColor() {	
+			const random = Math.floor(Math.random() * 5) + 1;
+			const colorChanges = ["$body", "$rightBtn", "$leftBtn", "$quoteText", "$authorText", "$marks"];
+			colorChanges.forEach((element, i) => {
+				view[element].removeClass().addClass((i <= 2 ? "color": "fontColor") + random); 
+			});
+		},
+		animate() {
+			const random = Math.floor(Math.random() * 8);
+			const animationArray = ["rotateIn", "zoomInDown","hinge","bounce","shake","rubberBand","swing","wobble"];
+			view.$main.addClass("animated " + animationArray[random]);
+			setTimeout(() => {
+				view.$main.removeClass("animated " + animationArray[random]);
+			}, 800);
+		},
+		tweet(string) {view.$twitter.attr("href", "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent(string));}
+	};
 
-$(document).ready(function() {
-	controller.init();
-})
+	$(document).ready(function() {
+		controller.init();
+	});
 })();
 
